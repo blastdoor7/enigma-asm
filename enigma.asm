8f846bd43ee034418a4f08ee63d4be94e367195e
blastdoor7 <51786421+blastdoor7@users.noreply.github.com>
2022-06-17 16:33:42 +0200

    1500 bytes

--- enigma.asm
+++ enigma.asm
@@ -128,17 +128,11 @@ _start:
     jmp configure_plug_board_loop
   configure_plug_board_loop_break:
 
-  jmp p16
-  padding16: db 67,79,78,70,73,71,88,88,88,88,88,88,88,88,88,88 
-  p16:
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ; Configure the rotors from the command line args
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   pusha
   mov ecx,dword [ebp-24]
-  jmp p17
-  padding17: db 67,79,78,70,73,71,00,77,79,86,00,68,76,88,88,88 
-  p17:
   mov dl,48
   mov bl,ROTOR_SEL_IDX
   call configure_rotors
@@ -204,10 +198,6 @@ _start:
       call step_rotors
       sub al,65
       call process_char
-      pusha 
-      mov al,32
-      call printchar
-      popa
       add al,65
       call printchar
       popa 
@@ -235,10 +225,6 @@ _start:
   process_char:
     mov bl,byte PLUGBOARD[eax]
     mov al,bl
-    add al,65
-    call printchar
-    sub al,65
-
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; right rotor
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -297,9 +283,6 @@ _start:
     mov ebx, dword ROTORS[edx]  ; select the rotor state
     push eax
     mov al, byte ebx[ROTOR_SEL_IDX]
-    ;add al,48
-    ;call printchar
-    ;sub al,48
     mov edx,dword ROTOR_ARRAYS[4*ecx]
     mov ecx,dword edx[4*eax]
     sub al,48
@@ -308,15 +291,11 @@ _start:
     call delta_mod_26
     mov dl,al
     pop eax
-    add eax,edx  
+    add al,dl    ; XXXXXX add eax,edx
     call modulo_26 
     push edx       
     mov dl,byte ecx[eax]  
     mov al,dl
-    pusha
-    add al,65
-    call printchar
-    popa
     pop edx       
     call delta_mod_26
     ret
@@ -390,10 +369,6 @@ _start:
     mov byte ROTOR_LEFT[ebx],al
     ret
 
-  ;padding0: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-  padding0_b: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-
-
   exit:
     mov esp,ebp
     pop ebp
@@ -406,135 +381,53 @@ _start:
   ; expected output :  PNORAUPMEWYUIFEZNEHEZWEUBU
 
   ; default - identity, gets configured with supplied plug board
-  align 4
   PLUGBOARD:        db 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  align 4
-                      ; 0, 1, 2,3, 4, 5, 6,7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  REFLECTOR_B:      db 24,17,20,7,16,18,11,3,15,23,13, 6,14,10,12, 8, 4, 1, 5,25, 2,22,21, 9, 0,19
-  align 4
+  REFLECTOR_B:      db 24,17,20,7,16,18,11,3,15,23,13,6,14,10,12,8,4,1,5,25,2,22,21,9,0,19
   REFLECTOR_C:      db 5,21,15,9,8,0,14,24,4,3,17,25,23,22,6,2,19,10,20,16,18,1,13,12,7,11
-  align 4
   REFLECTOR_B_thin: db 4,13,10,16,0,20,24,22,9,8,2,14,15,1,11,12,3,23,25,21,5,19,7,17,6,18
-  align 4
   REFLECTOR_C_thin: db 17,3,14,1,9,13,19,10,21,4,7,12,11,5,2,22,25,0,23,6,24,8,15,18,20,16
 
-  ;A B C D E F G H I J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y Z 
-  ;0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-  ; test value 432 B "AY BX DE FG QZ HP MW RS JV UT" CBA AAA A
-  ; expected output :  P
-  ;     A  ->  Y ->  
-  ;     I  ->  P -> III ->  IV  -> V   -> R  -> V  -> IV -> III -> P -> O
-  ;     0  -> 24 ->  14  -> 17 ->  0 ->  20 -> 10  -> 15->   8->   15  -> 15 P 
-  ;     A     Y      O      R      A     U     K      P      I     P
   ; forward rotor perms
-  align 4
-  padding_rI: db 82,79,84,79,82,0,73,0,65,82,82,65,89,88,70,88 
   ROTOR_I:          db 4,10,12,5,11,6,3,16,21,25,13,19,14,22,24,7,23,20,18,15,0,8,1,17,2,9
-  align 4
   ROTOR_II:         db 0,9,3,10,18,8,17,20,23,1,11,7,22,19,12,2,16,6,25,13,15,24,5,21,14,4
-  align 4
-                      ;0,1,2,3,4, 5,6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  ROTOR_III:        db 1,3,5,7,9,11,2,15,17,19,23,21,25,13,24, 4, 8,22, 6, 0,10,12,20,18,16,14
-  align 4
-                      ;0, 1, 2, 3, 4, 5,6,7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  ROTOR_IV:         db 4,18,14,21,15,25,9,0,24,16,20, 8,17, 7,23,11,13, 5,19, 6,10, 3, 2,12,22, 1
-  align 4
-                      ; 0, 1,2, 3,4,5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  ROTOR_V:          db 21,25,1,17,6,8,19,24,20,15,18, 3,13, 7,11,23, 0,22,12, 9,16,14, 5, 4, 2,10
-  align 4
-                      ;0, 1,2, 3, 4, 5, 6,7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  ROTOR_VI:         db 9,15,6,21,14,20,12,5,24,16, 1, 4,13, 7,25,17, 3,10, 0,18,23,11, 8, 2,19,22
-  align 4
+  ROTOR_III:        db 1,3,5,7,9,11,2,15,17,19,23,21,25,13,24,4,8,22,6,0,10,12,20,18,16,14
+  ROTOR_IV:         db 4,18,14,21,15,25,9,0,24,16,20,8,17,7,23,11,13,5,19,6,10,3,2,12,22,1
+  ROTOR_V:          db 21,25,1,17,6,8,19,24,20,15,18,3,13,7,11,23,0,22,12,9,16,14,5,4,2,10
+  ROTOR_VI:         db 9,15,6,21,14,20,12,5,24,16,1,4,13,7,25,17,3,10,0,18,23,11,8,2,19,22
   ROTOR_VII:        db 13,25,9,7,6,17,2,23,12,24,18,22,1,14,20,5,0,8,21,11,15,5,10,16,3,19
-  align 4
   ROTOR_VIII:       db 5,10,16,7,19,11,23,14,2,1,9,18,15,3,25,17,0,12,4,22,13,8,20,24,6,21
-  align 4
 
   ; reverse rotor perms
-  padding_rI_: db 82,79,84,79,82,0,73,0,65,82,82,65,89,88,82,88 
   ROTOR_I_REV:      db 20,22,24,6,0,3,5,15,21,25,1,4,2,10,12,19,7,23,18,11,17,8,13,16,14,9
-  align 4
   ROTOR_II_REV:     db 0,9,15,2,25,22,17,11,5,1,3,10,14,19,24,20,16,6,4,13,7,23,12,8,21,18
-  align 4
-                      ; 0,1,2,3, 4,5, 6,7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  ROTOR_III_REV:    db 19,0,6,1,15,2,18,3,16,4,20, 5,21,13,25, 7,24, 8,23, 9,22,11,17,10,16,12
-  align 4
-                      ;0, 1, 2, 3,4, 5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  ROTOR_IV_REV:     db 7,25,22,21,0,17,19,13,11,6,20,15,23,16, 2, 4, 9,12, 1,18,10, 3,24,16, 8, 5
-  align 4
-                      ; 0,1, 2, 3, 4, 5,6, 7,8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
-  ROTOR_V_REV:      db 16,2,24,11,23,22,4,13,5,19,25,16,18,12,21, 9,20, 3,10, 6, 8, 0,17,15, 7, 1
-  align 4
+  ROTOR_III_REV:    db 19,0,6,1,15,2,18,3,16,4,20,5,21,13,25,7,24,8,23,9,22,11,17,10,16,12
+  ROTOR_IV_REV:     db 7,25,22,21,0,17,19,13,11,6,20,15,23,16,2,4,9,12,1,18,10,3,24,16,8,5
+  ROTOR_V_REV:      db 16,2,24,11,23,22,4,13,5,19,25,16,18,12,21,9,20,3,10,6,8,0,17,15,7,1
   ROTOR_VI_REV:     db 18,10,23,16,11,7,2,13,22,0,17,21,6,12,4,1,9,15,19,24,5,3,25,20,8,14
-  align 4
   ROTOR_VII_REV:    db 16,12,6,24,21,15,4,3,17,2,22,19,8,0,13,20,23,5,10,25,14,18,11,7,9,1
-  align 4
   ROTOR_VIII_REV:   db 16,9,8,13,18,0,24,3,21,10,1,5,17,20,7,12,2,15,11,4,22,25,19,6,23,14
-  align 4
-  padding_rI1_: db 82,69,86,88,69,78,68,88,88,88,88,88,88,88,88,88 
-  padding4: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding5: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding6: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding7: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-
-  padding_rI2: db 82,69,70,88,65,82,82,65,89,88,88,88,88,88,88,88 
   REFLECTOR_ARRAY: dd REFLECTOR_B, REFLECTOR_C, REFLECTOR_B_thin, REFLECTOR_C_thin
-  padding_rI2_: db 82,69,70,88,65,82,82,65,89,88,69,78,68,88,88,88 
-  padding8: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding9: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding10: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding11: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  alignb 4
   REFLECTOR_NAME_B:    db 'B'
-  alignb 4
   REFLECTOR_NAME_C:    db 'C'
-  alignb 4
   REFLECTOR_NAMES_END: db  0
-  alignb 4
   REFLECTOR_NAMES: dd REFLECTOR_NAME_B,REFLECTOR_NAME_C,REFLECTOR_NAMES_END
-  alignb 4
-   
-  padding12: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding13: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding14: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 
-  padding15: db 82,79,84,79,82,0,65,82,82,65,89,88,88,88,88,88 
   ROTOR_ARRAY:     dd ROTOR_I,ROTOR_II,ROTOR_III,ROTOR_IV,ROTOR_V,ROTOR_VI,ROTOR_VII,ROTOR_VIII
-  alignb 4
   ROTOR_REV_ARRAY: dd ROTOR_I_REV,ROTOR_II_REV,ROTOR_III_REV,ROTOR_IV_REV,ROTOR_V_REV,ROTOR_VI_REV,ROTOR_VII_REV,ROTOR_VIII_REV
-  alignb 4
   STEPS_ARRAY:     dd ROTOR_I_STEPS,ROTOR_II_STEPS,ROTOR_III_STEPS,ROTOR_IV_STEPS,ROTOR_V_STEPS,ROTOR_VI_STEPS,ROTOR_VII_STEPS,ROTOR_VIII_STEPS
-  alignb 4
   ROTOR_RIGHT:  dd 0,0,0,0,0
-  alignb 4
   ROTOR_MIDDLE: dd 0,0,0,0,0
-  alignb 4
   ROTOR_LEFT:   dd 0,0,0,0,0  
-  alignb 4
   REFLECTOR:    dd 0
-  alignb 4
   ROTOR_I_STEPS:    db     16 
-  alignb 4
   ROTOR_II_STEPS:   db      4   ; 'F'
-  alignb 4
   ROTOR_III_STEPS:  db     21   ; 'W'     'V' -> 'W' step with change to 'W'
-  alignb 4
   ROTOR_IV_STEPS:   db      9   ; 'K'
-  alignb 4
   ROTOR_V_STEPS:    db     25   ; 'A'
-  alignb 4
   ROTOR_VI_STEPS:   db  25,12   ; 'AN'
-  alignb 4
   ROTOR_VII_STEPS:  db  25,12   ; 'AN'
-  alignb 4
   ROTOR_VIII_STEPS: db  25,12   ; 'AN'
-  alignb 4
-
-  padding_constants: db 88,88,88,88,88,88,88,88,88,88,88,88,88,88,88,88
   ROTOR_ARRAYS: dd ROTOR_ARRAY,ROTOR_REV_ARRAY
-  alignb 4
   ROTORS:       dd ROTOR_LEFT,ROTOR_MIDDLE,ROTOR_RIGHT
-  alignb 4
-  padding_end: db 69,78,68,0,79,70,0,68,65,84,65,88,88,88,88,88
 
   ROTOR_SEL_IDX                   equ   0
   ROTOR_RS_IDX                    equ   1
